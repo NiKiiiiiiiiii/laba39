20fe18a67451158138dca76d91408e682d8f738c
NIKitolas NIKitolas <ahundov.nikita@gmail.com>
2020-06-07 16:35:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laba31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
